cb218e5c98468dc7cb79b4730257fa98d665774e
Veltys <veltys@veltys.es>
2022-12-23 00:35:25 +0100

    ADD: Caché del tiempo

--- auxiliares/MOTDs/60-weather.sh
+++ auxiliares/MOTDs/60-weather.sh
@@ -3,14 +3,24 @@
 # Title         : 60-weather.sh
 # Description   : Muestra el tiempo en el MOTD
 # Author        : Veltys
-# Date          : 2022-12-21
-# Version       : 1.0.0
+# Date          : 2022-12-23
+# Version       : 2.0.0
 # Usage         : (llamado al iniciar sesión, si está correctamente instalado
 # Notes         : Es necesaria una terminal de 256 colores para apreciar el dibujo ASCII adecuadamente
 
 
+cache=/tmp/weather.cache
+
+command="curl \"es.wttr.in/?0&m\""
+
+exptime=21600 # 6 (horas) * 60 (minutos) * 60 (segundos)
+
 export TERM=xterm-256color
 
-curl "es.wttr.in/?0&m"
+if [ ! -f "$cache" ] || [ $(date +%s) -ge $(( $(stat -c %Y "$cache" 2> /dev/null || 0) + exptime )) ]; then
+        eval "$command" > "$cache"
+fi
+
+cat "$cache"
 
 echo
