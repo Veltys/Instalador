f2b80a5f64efef7c5d42f1ec9dfb1c2a7cde602f
Veltys <veltys@veltys.es>
2022-12-22 02:32:57 +0100

    FIX: Calidad de código

--- auxiliares/MOTDs/50-custom-motd.sh
+++ auxiliares/MOTDs/50-custom-motd.sh
@@ -3,8 +3,8 @@
 # Title         : 50-custom-motd.sh
 # Description   : Muestra un MOTD personalizado para Raspberries Pi
 # Author        : Veltys
-# Date          : 2022-12-21
-# Version       : 3.0.0
+# Date          : 2022-12-22
+# Version       : 3.0.1
 # Usage         : (llamado al iniciar sesión, si está correctamente instalado
 # Notes         : Es necesaria una terminal de 256 colores para apreciar el dibujo ASCII adecuadamente
 
@@ -13,26 +13,26 @@ export TERM=xterm-256color
 
 
 let upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
-let secs=$((${upSeconds}%60))
-let mins=$((${upSeconds}/60%60))
-let hours=$((${upSeconds}/3600%24))
-let days=$((${upSeconds}/86400))
-UPTIME=`printf "%d días, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs"`
+let secs=$((upSeconds%60))
+let mins=$((upSeconds/60%60))
+let hours=$((upSeconds/3600%24))
+let days=$((upSeconds/86400))
+UPTIME=$(printf "%d días, %02dh%02dm%02ds" "${days}" "${hours}" "${mins}" "${secs}")
 
 
-MEMFREE=`cat /proc/meminfo | grep MemFree | awk {'print $2'}`
-MEMTOTAL=`cat /proc/meminfo | grep MemTotal | awk {'print $2'}`
+MEMFREE=$(grep MemFree /proc/meminfo | awk {'print $2'})
+MEMTOTAL=$(grep MemTotal /proc/meminfo | awk {'print $2'})
 
 
-SDUSED=`df -h | grep 'dev/root' | awk '{print $3}' | xargs`
-SDAVAIL=`df -h | grep 'dev/root' | awk '{print $4}' | xargs`
+SDUSED=$(df -h | grep 'dev/root' | awk '{print $3}' | xargs)
+SDAVAIL=$(df -h | grep 'dev/root' | awk '{print $4}' | xargs)
 
 
 # get the load averages
 read one five fifteen rest < /proc/loadavg
 
 
-ips=( $(ip a | grep glo | awk '{print $2}' | cut -f1 -d/) )
+ips=( $(ip a | grep global | awk '{print $2}' | cut -f1 -d/) )
 
 declare ips_txt
 
@@ -63,14 +63,14 @@ NC="$(tput sgr0)" # No Color
 
 
 echo "${GREEN}
-   .~~.   .~~.    `date +"%A, %e %B %Y, %r"`
-  '. \ ' ' / .'   `uname -srmo`${RED}
+   .~~.   .~~.    $(date +"%A, %e %B %Y, %r")
+  '. \ ' ' / .'   $(uname -srmo)${RED}
    .~ .~~~..~.
   : .~.'~'.~. :   ${DARKGREY}Tiempo en línea..........: ${BLUE}${UPTIME}${RED}
  ~ (   ) (   ) ~  ${DARKGREY}Memoria..................: ${BLUE}${MEMFREE}kB (libre) / ${MEMTOTAL}kB (total)${RED}
 ( : '~'.~.'~' : ) ${DARKGREY}Uso de disco.............: ${BLUE}${SDUSED} (usado) / ${SDAVAIL} (libre)${RED}
  ~ .~ (   ) ~. ~  ${DARKGREY}Cargas de trabajo........: ${BLUE}${one}, ${five}, ${fifteen} (1, 5, 15 min)${RED}
-  (  : '~' :  )   ${DARKGREY}Procesos en ejecución....: ${BLUE}`ps ax | wc -l | tr -d " "`${RED}
+  (  : '~' :  )   ${DARKGREY}Procesos en ejecución....: ${BLUE}$(ps ax | wc -l | tr -d " ")${RED}
    '~ .~~~. ~'    ${DARKGREY}Direcciones IP...........: ${BLUE}${ips_txt} y ${ip_ext}${RED}
-       '~'        ${DARKGREY}Temperatura del sistema..: ${BLUE}`/usr/bin/vcgencmd measure_temp | sed -r -e "s/^temp=([0-9]*)\.([0-9])'C$/\1,\2 C/"`${NC}
+       '~'        ${DARKGREY}Temperatura del sistema..: ${BLUE}$(/usr/bin/vcgencmd measure_temp | sed -r -e "s/^temp=([0-9]*)\.([0-9])'C$/\1,\2 C/")${NC}
 "
